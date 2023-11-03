49ede81197d59e747064d62082470fb123c13992
cyan-utokyo <chenyan@iis.u-tokyo.ac.jp>
2023-11-03 21:59:50 +0900

    save

--- Procrustes_Elastic_PCA.py
+++ Procrustes_Elastic_PCA.py
@@ -77,6 +77,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.covariance import EmpiricalCovariance
 from scipy.stats import pearsonr
 import networkx as nx  # 导入NetworkX库
+from myvtk.MyRiemannCov import *
+
 warnings.filterwarnings("ignore")
 
 PCA_N_COMPONENTS = 16
@@ -168,6 +170,7 @@ print (Procrustes_curves.shape)
 i=30 # U
 j=46 # S
 
+log.write("- preprocessing_pca is not a SRVF PCA.\n")
 preprocessing_pca = PCAHandler(Procrustes_curves.reshape(len(Procrustes_curves),-1), None, 20, PCA_STANDARDIZATION)
 preprocessing_pca.PCA_training_and_test()
 preprocess_curves = preprocessing_pca.inverse_transform_from_loadings(preprocessing_pca.train_res).reshape(len(preprocessing_pca.train_res), -1, 3)
@@ -218,6 +221,16 @@ print(f"Procs_srvf_curves contains NaN values: {contains_nan}")
 # frechet_mean_srvf = frechet_mean_srvf / measure_length(frechet_mean_srvf)
 # 计算PCA
 # 保存数据
+
+def scale_dataset_to_unit_length(Procs_srvf_curves):
+    unit_procs_srvf_curves = np.zeros_like(Procs_srvf_curves)
+    for i in range(len(Procs_srvf_curves)):
+        unit_procs_srvf_curves[i] = Procs_srvf_curves[i] / measure_length(Procs_srvf_curves[i])
+    return unit_procs_srvf_curves
+
+unit_procs_srvf_curves = scale_dataset_to_unit_length(Procs_srvf_curves)
+
+
 log.write("PCA standardization: {}\n".format(PCA_STANDARDIZATION))
 print ("所有PCA的标准化状态：", PCA_STANDARDIZATION)
 all_srvf_pca = PCAHandler(Procs_srvf_curves.reshape(len(Procs_srvf_curves),-1), None, PCA_N_COMPONENTS, PCA_STANDARDIZATION)
@@ -591,99 +604,7 @@ for label in param_group_unique_labels:
 
 
 
-def draw_covariance_heatmap(data, savepath):
-    """
-    使用全部数据绘制协方差矩阵的热图。
-    
-    :param data: 形状为(m, n)的数据集，其中m是数据点的数量，n是特征的数量。
-    :param savepath: 图片保存路径。
-    """
-    scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(data)
-    
-    # 计算协方差矩阵
-    cov_matrix = EmpiricalCovariance().fit(data_scaled).covariance_
 
-    # 绘制热图
-    plt.figure(figsize=(8, 8), dpi=300)
-    sns.heatmap(cov_matrix, annot=False, cmap='viridis')
-    plt.title("Covariance Matrix Heatmap")
-    plt.tight_layout()
-    plt.savefig(savepath)
-    plt.close()
-
-
-def detect_linearly_related_groups(data, sample_size, correlation_threshold=0.95):
-    """
-    从数据集中挑选特定数量的数据点，计算协方差矩阵，检测并在结束时输出线性近似相关的团的总结。
-
-    :param data: 形状为(m, n)的数据集，其中m是数据点的数量，n是特征的数量。
-    :param sample_size: 要从数据中随机选取的样本数量。
-    :param correlation_threshold: 设定的相关系数阈值。
-    """
-    scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(data)
-    
-    m, n = data.shape
-    connected_components_summary = []
-
-    # 随机选择指定数量的数据点
-    selected_indices = np.random.choice(m, sample_size, replace=False)
-    selected_data = data_scaled[selected_indices]
-    
-    # 创建图来表示行之间的近似线性关系
-    G = nx.Graph()
-    for row_i in range(n):
-        G.add_node(row_i)
-        for row_j in range(row_i + 1, n):
-            corr, _ = pearsonr(selected_data[:, row_i], selected_data[:, row_j])
-            if abs(corr) > correlation_threshold:
-                G.add_edge(row_i, row_j)
-    
-    # 找到图中的所有连通分量（即线性近似相关的团或簇）
-    connected_components = list(nx.connected_components(G))
-
-    # 收集连通分量的信息以备最后输出
-    if connected_components:
-        connected_components_summary.extend(connected_components)
-
-    # 在所有操作执行完后打印总结
-    return connected_components_summary
-
-def repeated_detection(data, sample_size, repetitions, correlation_threshold=0.95):
-    """
-    多次执行 detect_linearly_related_groups 函数，并对结果进行统计。
-
-    :param data: 形状为(m, n)的数据集，其中m是数据点的数量，n是特征的数量。
-    :param sample_size: 要从数据中随机选取的样本数量。
-    :param repetitions: 执行 detect_linearly_related_groups 函数的次数。
-    :param correlation_threshold: 设定的相关系数阈值。
-    :return: 每次执行的统计结果。
-    """
-    overall_summary = []
-
-    for _ in range(repetitions):
-        summary = detect_linearly_related_groups(data, sample_size, correlation_threshold)
-        overall_summary.extend(summary)
-
-    # 统计结果
-    results_statistics = {}
-    for component in overall_summary:
-        if len(component) > 2:  # 只考虑长度大于2的组件
-            component = tuple(sorted(component))  # 对组件进行排序以保证一致性
-            if component in results_statistics:
-                results_statistics[component] += 1
-            else:
-                results_statistics[component] = 1
-
-    # 只保留出现次数至少为重复次数一半的组件
-    half_repetitions = repetitions // 2
-    filtered_results = {k: v for k, v in results_statistics.items() if v > half_repetitions}
-
-    # 按出现次数排序
-    sorted_results = dict(sorted(filtered_results.items(), key=lambda item: item[1], reverse=True))
-
-    return sorted_results
 
 # 举例使用
 # data = ... # 您的数据集
@@ -753,7 +674,7 @@ print ("Torsions statistics:", statistics)
 
 # 定义颜色映射
 colors = {
-    label: plt.cm.Set3((i)/(len(param_group_unique_labels))) for i, label in enumerate(param_group_unique_labels)
+    label: plt.cm.CMRmap((i+1)/(len(param_group_unique_labels)+1)) for i, label in enumerate(param_group_unique_labels)
 }
 
 
@@ -796,10 +717,10 @@ index = 0
 # 绘制散点图
 # 定义颜色映射
 Typevalues_colors = {
-    label: plt.cm.jet((i)/4) for i, label in enumerate(set(Typevalues))
+    label: plt.cm.gnuplot((i)/4) for i, label in enumerate(set(Typevalues))
 }
 param_group_colors = {
-    label: plt.cm.jet((i)/8) for i, label in enumerate(set(param_group))
+    label: plt.cm.gnuplot((i)/8) for i, label in enumerate(set(param_group))
 }
 total_curvature_energy = []
 total_torsion_energy = []
@@ -1149,33 +1070,27 @@ plt.close()
 ##########################################
 ##### 计算geodesic并评价线性相关性
 
-geodesic_dir = mkdir(bkup_dir, "geodesic")
-# FrechetMean = compute_frechet_mean(Procrustes_curves)
+# geodesic_dir = mkdir(bkup_dir, "geodesic")
+# # FrechetMean = compute_frechet_mean(Procrustes_curves)
 
-curves = compute_geodesic_shapes_between_two_curves(Procrustes_curves[0], Procrustes_curves[14], 10)
-print ("curves.shape:", curves.shape)
-# 获取 x, y, z 坐标
-x = curves[:, :, 0]
-y = curves[:, :, 1]
-z = curves[:, :, 2]
-
-# 创建一个新的图和3D轴
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# 使用线框图表示曲面
-# ax.plot_wireframe(x, y, z)
-ax.plot_surface(x, y, z, cmap='viridis', edgecolor='k')
-
-# 设置轴标签
-ax.set_xlabel('X Axis')
-ax.set_ylabel('Y Axis')
-ax.set_zlabel('Z Axis')
-
-# 显示图形
-plt.show()
-
-ArithmeticMean = np.mean(Procrustes_curves, axis=0)
+# curves = compute_geodesic_shapes_between_two_curves(Procrustes_curves[0], Procrustes_curves[14], 10)
+# print ("curves.shape:", curves.shape)
+# # 获取 x, y, z 坐标
+# x = curves[:, :, 0]
+# y = curves[:, :, 1]
+# z = curves[:, :, 2]
+# # 创建一个新的图和3D轴
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# # 使用线框图表示曲面
+# # ax.plot_wireframe(x, y, z)
+# ax.plot_surface(x, y, z, cmap='viridis', edgecolor='k')
+# # 设置轴标签
+# ax.set_xlabel('X Axis')
+# ax.set_ylabel('Y Axis')
+# ax.set_zlabel('Z Axis')
+# # 显示图形
+# plt.show()
 
 end_time = datetime.now()
 total_time = end_time - start_time
